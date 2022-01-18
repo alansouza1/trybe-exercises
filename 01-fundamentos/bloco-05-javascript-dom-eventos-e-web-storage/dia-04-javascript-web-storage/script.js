05277d530c5d773b1673361c926c484374603bf2
alansouza1 <alansouza6000@gmail.com>
2022-01-18 11:33:24 -0300

    Resolução do requisito 2

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
@@ -1,16 +1,39 @@
-const colors = ['#fff', '#f00', '#0f0', '#00f'];
+const colors = ['#fff', '#000', '#f00', '#0f0', '#00f'];
 const body = document.querySelector('body');
-const buttons = document.querySelectorAll('.change-background button');
+const backgroundColorButtons = document.querySelectorAll('.change-background-color button');
+const textColorButtons = document.querySelectorAll('.change-text-color button');
 
-for (let index = 0; index < buttons.length; index += 1) {
-  const button = buttons[index];
-  button.style.backgroundColor = colors[index];
-  button.addEventListener('click', changeBackgroundColor);
+function addColorToButtons(buttons) {
+  for (let index = 0; index < buttons.length; index += 1) {
+    const button = buttons[index];
+    button.style.backgroundColor = colors[index];
+  }
 }
+
+addColorToButtons(backgroundColorButtons);
+addColorToButtons(textColorButtons);
+
+function addEventToButtons(buttons, func) {
+  for (let index = 0; index < buttons.length; index += 1) {
+    const button = buttons[index];
+    button.addEventListener('click', func);
+  }
+}
+
+addEventToButtons(backgroundColorButtons, changeBackgroundColor);
+addEventToButtons(textColorButtons, changeTextColor);
 
 function changeBackgroundColor(event) {
   const button = event.target;
   const color = button.style.backgroundColor;
 
   body.style.backgroundColor = color;
+}
+
+function changeTextColor(event) {
+  const button = event.target;
+  const color = button.style.backgroundColor;
+
+  const text = document.querySelector('main');
+  text.style.color = color;
 }
