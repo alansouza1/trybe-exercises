413bc09ffb91d76830ad91d00e1209e6cc90f345
alansouza1 <alansouza6000@gmail.com>
2022-01-19 18:48:46 -0300

    Salva as preferências no localstorage

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
@@ -32,48 +32,135 @@ addEventToButtons(lineHeightButtons, changeLineHeight);
 addEventToButtons(fontFamilyButtons, changeFontFamily);
 
 function changeBackgroundColor(event) {
-  const button = event.target;
-  const color = button.style.backgroundColor;
+  let preferences = JSON.parse(localStorage.getItem('preferences'));
 
-  body.style.backgroundColor = color;
+  if (event !== undefined) {
+    const button = event.target;
+    const color = button.style.backgroundColor;
+  
+    body.style.backgroundColor = color;
+    preferences.backgroundColor = color;
+    localStorage.setItem('preferences', JSON.stringify(preferences));
+
+  } else {
+    const color = preferences.backgroundColor;
+    body.style.backgroundColor = color;
+  }
 }
 
 function changeTextColor(event) {
-  const button = event.target;
-  const color = button.style.backgroundColor;
+  let preferences = JSON.parse(localStorage.getItem('preferences'));
 
-  main.style.color = color;
+  if (event !== undefined) {
+    const button = event.target;
+    const color = button.style.backgroundColor;
+  
+    main.style.color = color;
+    preferences.textColor = color;
+    localStorage.setItem('preferences', JSON.stringify(preferences));
+  } else {
+    const color = preferences.textColor;
+    main.style.color = color;
+  }
+
 }
 
 function changeFontSize(event) {
-  const button = event.target;
-  const fontSize = button.innerText;
+  let preferences = JSON.parse(localStorage.getItem('preferences'));
 
-  main.style.fontSize = fontSize;
+  if (event !== undefined) {
+    const button = event.target;
+    const fontSize = button.innerText;
+  
+    main.style.fontSize = fontSize;
+    preferences.fontSize = fontSize;
+    localStorage.setItem('preferences', JSON.stringify(preferences));
+  } else {
+    const fontSize = preferences.fontSize;
+    main.style.fontSize = fontSize;
+  }
+
 }
 
 function changeLineHeight(event) {
-  const button = event.target;
-  const lineHeight = button.innerText;
+  let preferences = JSON.parse(localStorage.getItem('preferences'));
 
-  main.style.lineHeight = lineHeight;
+  if (event !== undefined) {
+    const button = event.target;
+    const lineHeight = button.innerText;
+  
+    main.style.lineHeight = lineHeight;
+    preferences.lineHeight = lineHeight;
+    localStorage.setItem('preferences', JSON.stringify(preferences));
+  } else {
+    const lineHeight = preferences.lineHeight;
+    main.style.lineHeight = lineHeight;
+  }
+  
 }
 
 function changeFontFamily(event) {
-  const button = event.target;
-  const fontFamily = button.innerText;
+  let preferences = JSON.parse(localStorage.getItem('preferences'));
 
-  switch (fontFamily) {
-    case 'Times New Roman':
-      main.style.fontFamily = "'Times New Roman', Times, serif";
-      break;
-    case 'Arial':
-      main.style.fontFamily = "Arial, Helvetica, sans-serif";
-      break;
-    case 'Courier New':
-      main.style.fontFamily = "'Courier New', Courier, monospace";
-      break;
-    default:
-      break;
+  if (event !== undefined) {
+    const button = event.target;
+    const fontFamily = button.innerText;
+  
+    switch (fontFamily) {
+
+      case 'Times New Roman':
+        main.style.fontFamily = "'Times New Roman', Times, serif";
+        preferences.fontFamily = 'Times New Roman';
+        localStorage.setItem('preferences', JSON.stringify(preferences));
+        break;
+
+      case 'Arial':
+        main.style.fontFamily = "Arial, Helvetica, sans-serif";
+        preferences.fontFamily = 'Arial';
+        localStorage.setItem('preferences', JSON.stringify(preferences));
+        break;
+
+      case 'Courier New':
+        main.style.fontFamily = "'Courier New', Courier, monospace";
+        preferences.fontFamily = 'Courier New';
+        localStorage.setItem('preferences', JSON.stringify(preferences));
+        break;
+        
+      default:
+        break;
+    }
+  } else {
+    const fontFamily = preferences.fontFamily;
+
+    switch (fontFamily) {
+
+      case 'Times New Roman':
+        main.style.fontFamily = "'Times New Roman', Times, serif";
+        break;
+
+      case 'Arial':
+        main.style.fontFamily = "Arial, Helvetica, sans-serif";
+        break;
+
+      case 'Courier New':
+        main.style.fontFamily = "'Courier New', Courier, monospace";
+        break;
+        
+      default:
+        break;
+    }
+  }
+
+}
+
+window.onload = function initialRenderization() {
+  if (localStorage.getItem('preferences') === null || localStorage.getItem('preferences') === '{}') {
+    localStorage.setItem('preferences', JSON.stringify({}));
+  } else {
+    changeBackgroundColor();
+    changeTextColor();
+    changeFontSize();
+    changeLineHeight();
+    changeFontFamily();
   }
 }
