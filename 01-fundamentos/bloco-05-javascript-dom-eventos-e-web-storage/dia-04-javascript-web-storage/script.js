990df578e7a4f1737ae3130aebde5b3dab993ae3
alansouza1 <alansouza6000@gmail.com>
2022-01-19 15:25:11 -0300

    Resolução do requisito 5

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
@@ -6,6 +6,7 @@ const backgroundColorButtons = document.querySelectorAll('.change-background-col
 const textColorButtons = document.querySelectorAll('.change-text-color button');
 const fontSizeButtons = document.querySelectorAll('.change-font-size button');
 const lineHeightButtons = document.querySelectorAll('.change-line-height button');
+const fontFamilyButtons = document.querySelectorAll('.change-font-family button');
 
 function addColorToButtons(buttons) {
   for (let index = 0; index < buttons.length; index += 1) {
@@ -28,6 +29,7 @@ addEventToButtons(backgroundColorButtons, changeBackgroundColor);
 addEventToButtons(textColorButtons, changeTextColor);
 addEventToButtons(fontSizeButtons, changeFontSize);
 addEventToButtons(lineHeightButtons, changeLineHeight);
+addEventToButtons(fontFamilyButtons, changeFontFamily);
 
 function changeBackgroundColor(event) {
   const button = event.target;
@@ -55,4 +57,23 @@ function changeLineHeight(event) {
   const lineHeight = button.innerText;
 
   main.style.lineHeight = lineHeight;
+}
+
+function changeFontFamily(event) {
+  const button = event.target;
+  const fontFamily = button.innerText;
+
+  switch (fontFamily) {
+    case 'Times New Roman':
+      main.style.fontFamily = "'Times New Roman', Times, serif";
+      break;
+    case 'Arial':
+      main.style.fontFamily = "Arial, Helvetica, sans-serif";
+      break;
+    case 'Courier New':
+      main.style.fontFamily = "'Courier New', Courier, monospace";
+      break;
+    default:
+      break;
+  }
 }
