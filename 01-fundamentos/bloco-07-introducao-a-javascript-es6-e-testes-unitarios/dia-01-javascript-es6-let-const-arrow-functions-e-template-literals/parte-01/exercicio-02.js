a51eaf4773969743ef82610e4369698f7ce59131
alansouza1 <alansouza6000@gmail.com>
2022-02-11 17:28:07 -0300

    Resolução do exercício bônus da parte 1

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/parte-01/exercicio-02.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/parte-01/exercicio-02.js
@@ -1,18 +1,20 @@
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-const orderArray = (array) => {
-  const result = array;
-  for (let index = 0; index < result.length; index += 1) {
-    for (let index2 = 0; index2 < result.length; index2 += 1) {
-      const number = result[index2];
-      const nextNumber = result[index2 + 1];
-      if (number > nextNumber) {
-        result[index2] = nextNumber;
-        result[index2 + 1] = number;
-      }
-    }
-  }
-  return result;
-}
+// const orderArray = array => {
+//   const result = array;
+//   for (let index = 0; index < result.length; index += 1) {
+//     for (let index2 = 0; index2 < result.length; index2 += 1) {
+//       const number = result[index2];
+//       const nextNumber = result[index2 + 1];
+//       if (number > nextNumber) {
+//         result[index2] = nextNumber;
+//         result[index2 + 1] = number;
+//       }
+//     }
+//   }
+//   return result;
+// }
+
+const orderArray = array => array.sort((a,b) => a - b);
 
 console.log(`Os números ${orderArray(oddsAndEvens)} se encontram ordenados de forma crescente!`);
