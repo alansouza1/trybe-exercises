7b5d5e9a6f54a7a6db7e991c75052bf827a0edde
alansouza1 <alansouza6000@gmail.com>
2022-02-18 22:46:34 -0300

    Resolução do exercício 3

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/exercicio-02/myRemove.test.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/exercicio-02/myRemove.test.js
@@ -6,10 +6,12 @@ describe('A função myRemove(arr, item) recebe um array arr e retorna uma cópi
     const result = [1, 2, 4];
     expect(myRemove(array, 3)).toEqual(result);
   });
+
   it('Verifica se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
     const array = [1, 2, 3, 4];
     expect(myRemove(array, 3)).not.toEqual(array);
   });
+  
   it('Verifica se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
     const array = [1, 2, 3, 4];
     expect(myRemove(array, 5)).toEqual(array);
