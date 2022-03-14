8eeff429b48506b1f74b410938864fa418e216a0
alansouza1 <alansouza6000@gmail.com>
2022-03-13 21:03:55 -0300

    Resolução dos exercícios 3 e 4 dia 11.2

--- 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/my-form-2.0/src/Form.js
+++ 02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/my-form-2.0/src/Form.js
@@ -9,6 +9,8 @@ class Form extends React.Component {
     this.handleError = this.handleError.bind(this);
     this.handleBlur = this.handleBlur.bind(this);
     this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.showData = this.showData.bind(this);
+    this.clearAll = this.clearAll.bind(this);
 
     this.state = {
       name: '',
@@ -18,11 +20,55 @@ class Form extends React.Component {
       city: '',
       state: '',
       type: '',
-      formWithErrors: false,
+      formWithErrors: true,
       resume: '',
       post: '',
       description: '',
       hasAlerted: false,
+      data: '',
+    }
+  }
+
+  clearAll() {
+    this.setState({
+      name: '',
+      email: '',
+      cpf: '',
+      address: '',
+      city: '',
+      state: '',
+      type: '',
+      formWithErrors: true,
+      resume: '',
+      post: '',
+      description: '',
+      data: '',
+    })
+  }
+
+  showData() {
+    const { name, email, cpf, address, city, state, type, formWithErrors, resume, post, description } = this.state;
+    if (!formWithErrors) {
+      this.setState({
+        data: (
+          <div>
+            <p>Nome: {name}</p>
+            <p>Email: {email}</p>
+            <p>CPF: {cpf}</p>
+            <p>Endereço: {address}</p>
+            <p>Cidade: {city}</p>
+            <p>Estado: {state}</p>
+            <p>Tipo: {type}</p>
+            <p>Resumo do currículo: {resume}</p>
+            <p>Cargo: {post}</p>
+            <p>Descrição do cargo: {description}</p>
+          </div>
+        ),
+      })
+    } else {
+      this.setState({
+        data: (<span>Preencha todos os campos!</span>),
+      })
     }
   }
 
@@ -72,7 +118,7 @@ class Form extends React.Component {
   handleChange({ target }) {
     const { name, type } = target;
     let value = type === 'checkbox' ? target.checked : target.value;
-    if (name === 'email' && value.length > 0) {
+    if (name === 'address' && value.length > 0) {
       const numbersAndLetters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
       const hasNoSpecialCharacter = numbersAndLetters.find((character) => character === value[value.length - 1].toLowerCase());
 
@@ -89,92 +135,98 @@ class Form extends React.Component {
   }
 
   render() {
-    const { name, email, cpf, address, city, state, resume, post, description } = this.state;
+    const { name, email, cpf, address, city, state, resume, post, description, data } = this.state;
 
     return (
-      <form>
-        <fieldset className='form'>
-          <legend>Preencha:</legend>
-          <label htmlFor='name'>
-            Nome:
-            <input id='name' name='name' type='text' value={name} onChange={this.handleChange} />
-          </label>
-          <label htmlFor='email'>
-            Email:
-            <input id='email' name='email' type='text' value={email} onChange={this.handleChange} />
-          </label>
-          <label htmlFor='cpf'>
-            CPF:
-            <input id='cpf' name='cpf' type='text' value={cpf} onChange={this.handleChange} />
-          </label>
-          <label htmlFor='address'>
-            Endereço:
-            <input id='address' name='address' type='text' value={address} onChange={this.handleChange} />
-          </label>
-          <label htmlFor='city'>
-            Cidade:
-            <input id='city' name='city' type='text' value={city} onChange={this.handleChange} onBlur={this.handleBlur} />
-          </label>
-          <label htmlFor='state'>
-            Estado:
-            <select id='state' name='state' value={state} onChange={this.handleChange}>
-              <option value='ac'>Acre</option>
-              <option value='al'>Alagoas</option>
-              <option value='ap'>Amapá</option>
-              <option value='am'>Amazonas</option>
-              <option value='ba'>Bahia</option>
-              <option value='ce'>Ceará</option>
-              <option value='df'>Distrito Federal</option>
-              <option value='es'>Espírito Santo</option>
-              <option value='go'>Goiás</option>
-              <option value='ma'>Maranhão</option>
-              <option value='mt'>Mato Grosso</option>
-              <option value='ms'>Mato Grosso do Sul</option>
-              <option value='mg'>Minas Gerais</option>
-              <option value='pa'>Pará</option>
-              <option value='pb'>Paraíba</option>
-              <option value='pr'>Paraná</option>
-              <option value='pe'>Pernambuco</option>
-              <option value='pi'>Piauí</option>
-              <option value='rj'>Rio de Janeiro</option>
-              <option value='rn'>Rio Grande do Norte</option>
-              <option value='rs'>Rio Grande do Sul</option>
-              <option value='ro'>Rondônia</option>
-              <option value='rr'>Roraima</option>
-              <option value='sc'>Santa Catarina</option>
-              <option value='sp'>São Paulo</option>
-              <option value='se'>Sergipe</option>
-              <option value='to'>Tocantins</option>
-            </select>
-          </label>
-          <fieldset>
-            <legend>Tipo</legend>
-            <label htmlFor='house'>
-              <input name='type' id='house' type='radio' value='house' onChange={this.handleChange} />
-              Casa
+      <div>
+        <form>
+          <fieldset className='form'>
+            <legend>Preencha:</legend>
+            <label htmlFor='name'>
+              Nome:
+              <input id='name' name='name' type='text' value={name} onChange={this.handleChange} />
             </label>
-            <label htmlFor='ap'>
-              <input name='type' id='ap' type='radio' value='ap' onChange={this.handleChange} />
-              Apartamento
+            <label htmlFor='email'>
+              Email:
+              <input id='email' name='email' type='text' value={email} onChange={this.handleChange} />
+            </label>
+            <label htmlFor='cpf'>
+              CPF:
+              <input id='cpf' name='cpf' type='text' value={cpf} onChange={this.handleChange} />
+            </label>
+            <label htmlFor='address'>
+              Endereço:
+              <input id='address' name='address' type='text' value={address} onChange={this.handleChange} />
+            </label>
+            <label htmlFor='city'>
+              Cidade:
+              <input id='city' name='city' type='text' value={city} onChange={this.handleChange} onBlur={this.handleBlur} />
+            </label>
+            <label htmlFor='state'>
+              Estado:
+              <select id='state' name='state' value={state} onChange={this.handleChange}>
+                <option value='Acre'>Acre</option>
+                <option value='Alagoas'>Alagoas</option>
+                <option value='Amapá'>Amapá</option>
+                <option value='Amazonas'>Amazonas</option>
+                <option value='Bahia'>Bahia</option>
+                <option value='Ceará'>Ceará</option>
+                <option value='Distrito Federal'>Distrito Federal</option>
+                <option value='Espírito Santo'>Espírito Santo</option>
+                <option value='Goiás'>Goiás</option>
+                <option value='Maranhão'>Maranhão</option>
+                <option value='Mato Grosso'>Mato Grosso</option>
+                <option value='Mato Grosso do Sul'>Mato Grosso do Sul</option>
+                <option value='Minas Gerais'>Minas Gerais</option>
+                <option value='Pará'>Pará</option>
+                <option value='Paraíba'>Paraíba</option>
+                <option value='Paraná'>Paraná</option>
+                <option value='Pernambuco'>Pernambuco</option>
+                <option value='Piauí'>Piauí</option>
+                <option value='Rio de Janeiro'>Rio de Janeiro</option>
+                <option value='Rio Grande do Norte'>Rio Grande do Norte</option>
+                <option value='Rio Grande do Sul'>Rio Grande do Sul</option>
+                <option value='Rondônia'>Rondônia</option>
+                <option value='Roraima'>Roraima</option>
+                <option value='Santa Catarina'>Santa Catarina</option>
+                <option value='São Paulo'>São Paulo</option>
+                <option value='Sergipe'>Sergipe</option>
+                <option value='Tocantins'>Tocantins</option>
+              </select>
+            </label>
+            <fieldset>
+              <legend>Tipo</legend>
+              <label htmlFor='house'>
+                <input name='type' id='house' type='radio' value='Casa' onChange={this.handleChange} />
+                Casa
+              </label>
+              <label htmlFor='ap'>
+                <input name='type' id='ap' type='radio' value='Apartamento' onChange={this.handleChange} />
+                Apartamento
+              </label>
+            </fieldset>
+          </fieldset>
+          <fieldset className='form'>
+            <legend>Último Emprego</legend>
+            <label htmlFor='resume'>
+              Resumo do currículo:
+              <textarea name='resume' id='resume' value={resume} onChange={this.handleChange} />
+            </label>
+            <label htmlFor='post'>
+              Cargo:
+              <textarea name='post' id='post' value={post} onChange={this.handleChange} onMouseEnter={this.handleMouseEnter} />
+            </label>
+            <label htmlFor='description'>
+              Descrição do cargo:
+              <input type='text' name='description' id='description' value={description} onChange={this.handleChange} />
             </label>
           </fieldset>
-        </fieldset>
-        <fieldset className='form'>
-          <legend>Último Emprego</legend>
-          <label htmlFor='resume'>
-            Resumo do currículo:
-            <textarea name='resume' id='resume' value={resume} onChange={this.handleChange} />
-          </label>
-          <label htmlFor='post'>
-            Cargo:
-            <textarea name='post' id='post' value={post} onChange={this.handleChange} onMouseEnter={this.handleMouseEnter} />
-          </label>
-          <label htmlFor='description'>
-            Descrição do cargo:
-            <input type='text' name='description' id='description' value={description} onChange={this.handleChange} />
-          </label>
-        </fieldset>
-      </form>
+        </form>
+        <button onClick={ this.showData }>Mostrar Dados</button>
+        <button onClick={ this.clearAll }>Limpar</button>
+        <br/>
+        { data }
+      </div>
     );
   }
 }
